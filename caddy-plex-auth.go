007c6a079149755d32f19de346e789bc53bc46db
Alec Wiese <1350676+AlecWiese@users.noreply.github.com>
2025-02-16 17:41:43 -0700

    refactor: simplify Caddy module ID for clarity

--- caddy-plex-auth.go
+++ caddy-plex-auth.go
@@ -36,7 +36,7 @@ func init() {
 // CaddyModule returns the Caddy module information.
 func (Provider) CaddyModule() caddy.ModuleInfo {
 	return caddy.ModuleInfo{
-		ID:  "http.handlers.plex_auth",
+		ID:  "plex_auth",
 		New: func() caddy.Module { return &Provider{new(PlexOverseerrHandler)} },
 	}
 }
